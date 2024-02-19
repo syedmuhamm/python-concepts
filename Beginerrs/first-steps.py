48004ab49c84e8301a56e09415e3c29814f721f8
syedmuhammad <syedmuhammadzulqarnain2@gmail.com>
2024-02-19 18:53:36 +0100

    Getting used to other simple concepts, i.e. while, if, & for loops.

--- Beginerrs/first-steps.py
+++ Beginerrs/first-steps.py
@@ -1,6 +1,32 @@
 # Simultanous assignments in Python, used twice
-a , b = 0 ,1
+print("Simultanous assignments")
+a, b = 0, 1
 while(a < 10):
     print(a)
     a, b = b, a+b
    
+# Not skipping line with end, since print statement prints in new line
+print("Not skipping line with end")
+a, b = 0, 1
+while(a < 10):
+    print(a , end=",")
+    a, b = b, a+b
+
+# Getting used to input and if condition, no checks for non integer value ( for now)
+print("\nInput and if condition")
+x = int(input("Please enter an interger: "))
+if(x < 0):
+    x=0
+    print('Negative value changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+# For loop while measuring strings length
+print('\nFor loops xmeasuring strings length\n')
+words = ['Tea', 'Peacock', 'Bugs']
+for eachWord in words:
+    print(eachWord, len(eachWord))
